acbcf357e40da460abea22484630103907a2e527
Hamza <hamzaaddami11@gmail.com>
2025-10-18 14:38:51 +0100

    first c

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default function App() {
+
+  const me = {
+    nom: 'Addami',
+    prenom: 'Hamza',
+    date: new Date().toLocaleDateString(),
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Image
+          source={require('./assets/emsilog.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.title}>EMSI MAARIF</Text>
+      </View>
+
+      <View style={styles.card}>
+        <Text style={styles.label}>Nom : <Text style={styles.value}>{me.nom}</Text></Text>
+        <Text style={styles.label}>Prénom : <Text style={styles.value}>{me.prenom}</Text></Text>
+        <Text style={styles.label}>Date : <Text style={styles.value}>{me.date}</Text></Text>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +31,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 60,
+  },
+  logo: {
+    width: 200,
+    height: 55,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#2C786C',
+    marginLeft: 10,
+  },
+  card: {
+    width: '80%',
+    backgroundColor: '#f2f2f2', 
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  value: {
+    fontWeight: '600',
+    color: '#333',
   },
 });
