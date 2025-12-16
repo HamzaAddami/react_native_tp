0b595f3353578ff8d25341544a7df38fd95cf79b
Hamza <hamzaaddami11@gmail.com>
2025-12-16 23:51:59 +0100

    TP_5

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./navigation/AppNavigator";
+import AuthProvider, { AuthContext } from "./context/AuthContext";
+import AppDrawer from "./navigation/AppDrawer";
+import LoginScreen from "./screens/LoginScreen";
 
-export default function App() {
+
+function RootNavigator() {
+    const { user } = React.useContext(AuthContext);
+    return user ? <AppDrawer /> : <LoginScreen />;
+  }
+
+
+// Main App pour le TP_4
+/* export default function App() {
   return (
     <NavigationContainer>
       <AppNavigator />
     </NavigationContainer>
+  );
+}*/
+
+// Main App pour le TP_5
+export default function App() {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
